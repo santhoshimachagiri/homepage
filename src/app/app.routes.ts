cdc05092ea3a01c387aff31015a891e2b37a5be9
santhoshimachagiri <santhoship.454@example.com>
2024-09-28 00:38:50 +0530

    Initialise

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:HomeComponent},
+    { path:'login',component:LoginComponent},
+     {path:'signup',component:SignupComponent}
+];
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+
+})
+export class AppRoutingModule{}
